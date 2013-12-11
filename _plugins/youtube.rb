384cc6ba802fa681696bf81fe4d0d975becf9ccf
Owen Wang <wangfowen@gmail.com>
2013-12-11 22:42:57 +0100

    fix youtube plugin

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -6,14 +6,6 @@ class YouTube < Liquid::Tag
 
     if markup =~ Syntax then
       @id = $1
-
-      if $2.nil? then
-          @width = 560
-          @height = 420
-      else
-          @width = $2.to_i
-          @height = $3.to_i
-      end
     else
       raise "No YouTube ID provided in the \"youtube\" tag"
     end
@@ -21,7 +13,7 @@ class YouTube < Liquid::Tag
 
   def render(context)
     # "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\"allowfullscreen></iframe>"
-    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}?color=white&theme=light\"></iframe>"
+    "<div class=\"video\"><iframe src=\"http://www.youtube.com/embed/#{@id}\" allowfullscreen frameborder=\"0\"></iframe></div>"
   end
 
   Liquid::Template.register_tag "youtube", self
